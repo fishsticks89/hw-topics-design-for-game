74bcbe3fa8cf634c7f69136908e612489f51e730
Cdog135 <ccaras1@hwemail.com>
2024-12-06 11:46:51 -0800

    tried to implement the unicorns

--- src/routes/Field.svelte
+++ src/routes/Field.svelte
@@ -41,9 +41,51 @@
     background: white;
     border-radius: 50%;
   }
+  .unicorn {
+    position: absolute;
+    width: 80px;
+    height: 80px;
+    opacity: 0.2;
+    pointer-events: none;
+  }
+  .unicorn-1 {
+    top: 20%;
+    left: 20%;
+  }
+  .unicorn-2 {
+    bottom: 20%;
+    right: 20%;
+  }
+  .unicorn svg {
+    width: 100%;
+    height: 100%;
+  }
 </style>
 
 <div class="pong-square" style="--size: {squareSize}px">
+  <!-- Decorative Unicorns -->
+  <div class="unicorn unicorn-1">
+    <svg viewBox="0 0 100 100" fill="none" stroke="white" stroke-width="2">
+      <!-- Simplified unicorn design 1 -->
+      <path d="M30 70 L45 65 L60 70 L70 60 L75 45 L70 30"/>
+      <path d="M70 30 L60 20 L45 15 L30 20"/>
+      <path d="M45 15 L40 5"/>
+      <path d="M60 70 L55 85 L60 95"/>
+      <path d="M45 65 L40 85 L45 95"/>
+    </svg>
+  </div>
+  
+  <div class="unicorn unicorn-2">
+    <svg viewBox="0 0 100 100" fill="none" stroke="white" stroke-width="2">
+      <!-- Simplified unicorn design 2 (mirrored) -->
+      <path d="M70 70 L55 65 L40 70 L30 60 L25 45 L30 30"/>
+      <path d="M30 30 L40 20 L55 15 L70 20"/>
+      <path d="M55 15 L60 5"/>
+      <path d="M40 70 L45 85 L40 95"/>
+      <path d="M55 65 L60 85 L55 95"/>
+    </svg>
+  </div>
+
   <!-- Top Paddle -->
   <div
     class="paddle paddle-top"
